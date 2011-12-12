f11e05c8375add9ce841cafce1d49e322acf3245
Geoff Vedernikoff <yefim323@gmail.com>
2011-12-12 15:34:47 -0500

    fixed Feedback Close button

--- media/front-end/js/uservoice.js
+++ media/front-end/js/uservoice.js
@@ -1,22 +1,6 @@
-var uservoiceOptions = {
-  /* required */
-  key: 'pennappslabs',
-  host: 'pennappslabs.uservoice.com',
-  forum: '98069',
-  showTab: true,
-  /* optional */
-  alignment: 'left',
-  background_color:'#f00',
-  text_color: 'white',
-  hover_color: '#06C',
-  lang: 'en'
-};
-function _loadUserVoice() {
-  var s = document.createElement('script');
-  s.setAttribute('type', 'text/javascript');
-  s.setAttribute('src', ("https:" == document.location.protocol ? "https://" : "http://") + "cdn.uservoice.com/javascripts/widgets/tab.js");
-  document.getElementsByTagName('head')[0].appendChild(s);
-}
-_loadSuper = window.onload;
-window.onload = (typeof window.onload != 'function') ? _loadUserVoice : function() { _loadSuper(); _loadUserVoice(); };
-
+var uvOptions = {};
+(function() {
+  var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+  uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/4N3cwvYW0AWyJB3FHP95tQ.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+})();
