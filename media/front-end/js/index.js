5e2f95cf867e56bae149816d1c611c7e1be2795f
Kyle Hardgrave <kyleh@sas.upenn.edu>
2012-02-09 20:16:43 -0500

    Moved analytics back to main HTML

--- media/front-end/js/index.js
+++ media/front-end/js/index.js
@@ -17,17 +17,6 @@ function bookmarkSite(title, url) {
 		alert('Bookmarking is easy! Press Ctrl+D to bookmark.');
 }
 
-<!-- Google Analytics -->
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-21029575-2']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 var yellowIcon = "http://maps.google.com/mapfiles/ms/micons/yellow.png";
 var redIcon = "http://maps.google.com/mapfiles/ms/micons/red.png";
 var blueIcon = "http://maps.google.com/mapfiles/ms/micons/blue.png";
