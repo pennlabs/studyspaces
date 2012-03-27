cc79f8344cfaef4c4b6c9488c69d86f40ca8ad15
Matthew Croop <matthewcroop@gmail.com>
2012-03-26 21:08:11 -0400

    fixed django deprecationwarning

--- settings.py
+++ settings.py
@@ -13,12 +13,16 @@ SERVER_EMAIL="pennapps@ve.rckr5ngx.vesrv.com"
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'studyspaces'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'studyspaces'             # Not used with sqlite3.
-#DATABASE_PASSWORD = None         # Not used with sqlite3. SET IN SANDBOX_CONFIG
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'studyspaces',             # Or path to database file if using sqlite3.
+        'USER': 'studyspaces',             # Not used with sqlite3.
+        'PASSWORD': DATABASE_PASSWORD,         # Not used with sqlite3. SET IN SANDBOX_CONFIG
+        'HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',             # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -56,9 +60,9 @@ ADMIN_MEDIA_PREFIX = '/%s/media/' % (DISPLAY_NAME)
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-     'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
